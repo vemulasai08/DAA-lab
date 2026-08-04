5b18bc3f5ff3e429246a4d13d5cf098a954a8f8a
vemulasai08 <vemulasai690@gmail.com>
2026-08-04 23:04:53 +0530

    Create quick_sort.py

---
+++ practical 1/quick_sort.py
@@ -0,0 +1,17 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quick_sort(left) + middle + quick_sort(right)
+
+arr = list(map(int, input("Enter numbers separated by space: ").split()))
+
+sorted_arr = quick_sort(arr)
+
+print("Sorted array:", sorted_arr)
